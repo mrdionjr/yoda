5e2e13a23b49c245c582f2af23387c1e7e6b8c63
Salomon Dion <dev.mrdion@gmail.com>
2018-12-03 00:07:54 +0000

    Add tests

--- tests/TemplateParserTest.php
+++ tests/TemplateParserTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Yoda\MissingVariableException;
+use Yoda\Parsers\CsvParser;
 use Yoda\Template;
 use Yoda\TemplateParser;
 use PHPUnit\Framework\TestCase;
@@ -17,19 +18,29 @@ class TemplateParserTest extends TestCase
 
     public function test_handle_content_from_file()
     {
-        $template = new Template(__DIR__ . '/template-test.txt', ['name' => 'Yoda']);
+        $template = new Template(__DIR__ . '/template.txt', ['name' => 'Yoda']);
         $this->assertRegExp('/Yoda/', TemplateParser::parse($template));
     }
 
     public function test_should_handle_custom_delimiter()
     {
         $template = new Template('<% name %>', ['name' => 'Yoda']);
-        $this->assertEquals('Yoda', TemplateParser::parse($template, '<%', '%>'));
+        $template->setDelimiters('<%', '%>');
+        $this->assertEquals('Yoda', TemplateParser::parse($template));
+    }
+
+    public function test_should_handle_csv_file()
+    {
+        $template = new Template(__DIR__ . '/template.txt');
+        TemplateParser::use(new CsvParser());
+        $result = TemplateParser::parse($template, ['csv' => __DIR__ . '/template.csv']);
+        $this->assertInternalType('array', $result);
+        $this->assertRegExp('/Yoda/', $result[0]);
     }
 
     public function test_should_throw_exception_if_missing_variable()
     {
-        $template = new Template('{{ name }}', []);
+        $template = new Template('{{ name }}');
         $this->expectException(MissingVariableException::class);
         TemplateParser::parse($template);
     }
